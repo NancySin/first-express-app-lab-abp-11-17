7eb9ae7d0ab0ca9e603452e1ec972eb4544f67ce
nancy sin <nsin@sbcglobal.net>
2017-12-27 03:33:55 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var app = express()
 
 // Build Your Route Here
 app.get('/', function( req, resp){
+<<<<<<< HEAD
   resp.send('Hello, World!')
+=======
+  resp.send('Hello World!')
+>>>>>>> cf60ac719b5075431011ce9a7a432244963e5117
 
 })
 
